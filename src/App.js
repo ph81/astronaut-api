bc3009207a31c03cb43694a4aeb6ced4d29ecb9c
Edith M <paperhearts81@gmail.com>
2021-12-08 20:32:35 -0600

    star wars api added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   const [astronauts, setAstronauts] = useState([]);
   const api_url = 'http://api.open-notify.org/astros.json';
+  const [characters, setCharacters] = useState([]);
+  const star_wars = 'https://swapi.dev/api/people';
 
   // setting the dependencies array in useEffect to an empty array
   // ensures this fetch request only runs once instead of infinitely
@@ -26,6 +29,26 @@ const App = () => {
       <p>{astronaut.name} 👩‍🚀 is flying in the {astronaut.craft} 🚀 spacecraft</p>
     </li>
   ));
+  
+  // Star Wars
+  useEffect(() => {
+    const source = axios.CancelToken.source(); 
+    axios.get(star_wars, { cancelToken: source.token})
+    .then((response) => {
+      setCharacters(response.data.results);
+    })
+    .catch((error) => {
+      if (axios.isCancel(error)) return;
+    });
+
+    return () => source.cancel();
+  }, []);
+
+  const characterList = characters.map((char, index) => (
+    <li key={index}>
+      <p>{char.name} - Birth in {char.birth_year}</p>
+    </li>
+  ))
 
   return (
     <div className='App'>
@@ -33,6 +56,10 @@ const App = () => {
         <h2>Astronauts</h2>
         <ul>
           {astronautList}
+        </ul>
+        <h3>Star Wars characters</h3>
+        <ul>
+          {characterList}
         </ul>
       </main>
      
